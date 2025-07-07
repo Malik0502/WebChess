5b04dcbccfe3906ebd06cd12a3f2533f7f1981eb
Malik Sander <maliksander0502@icloud.com>
2025-07-07 17:01:58 +0200

    Fix: gameBoard available outside of window.onload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { GameBoardCreator } from "./scripts/board/chessBoardCreator"
 import { PieceFactory } from "./scripts/game/pieces/pieceFactory"
 
-window.onload = () => {
-    const gameBoard : GameBoardCreator = new GameBoardCreator(document.getElementById("game-canvas") as HTMLCanvasElement, "#F0D9B5", "#B58863", new PieceFactory())
+let gameBoard  : GameBoardCreator;
 
+window.onload = () => {
+    gameBoard = new GameBoardCreator(document.getElementById("game-canvas") as HTMLCanvasElement, "#F0D9B5", "#B58863", new PieceFactory())
     gameBoard.drawChessBoard();
 }
