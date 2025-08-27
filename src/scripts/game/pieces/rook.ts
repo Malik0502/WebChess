7dd44f2dab09be59b3c3f44f1baf065f0f509b6d
Malik0502 <65547172+Malik0502@users.noreply.github.com>
2025-08-27 20:59:10 +0200

    Feat: added rook movement to the north

--- src/scripts/game/pieces/rook.ts
+++ src/scripts/game/pieces/rook.ts
@@ -27,10 +27,51 @@ export class Rook implements IPiece{
     }
     
     calcPossibleMoves(board: GameTile[][]){
-        throw new Error("Method not implemented.");
+        this.possibleMoves = [];
+        var rookRow: number = this.currentTile.row;
+        var rookCol: number = this.currentTile.col;
+
+        this.CalcMovesNorth(board, rookRow, rookCol);
+        this.CalcMovesSouth(board, rookRow, rookCol);
+        this.CalcMovesEast(board, rookRow, rookCol);
+        this.CalcMovesWest(board, rookRow, rookCol);
+        
+        this.markAsMoveOption();
+        console.log(this.possibleMoves);
     }
 
-    markAsMoveOption(): void {
-        throw new Error("Method not implemented.");
+    private CalcMovesNorth(board: GameTile[][], rookRow: number, rookCol: number){
+        for (let index = rookRow; index >= 0; index--) {
+            if(index == rookRow) continue;
+
+            const element: GameTile = board[index][rookCol];
+
+            if(element.currentPiece && element.currentPiece.color == this.color) break;
+
+            if(element.isOccupied && element.currentPiece!.color != this.color){
+                this.possibleMoves.push(element);
+                break;
+            }
+
+            if(!element.isOccupied) this.possibleMoves.push(element);
+        }
+    }
+
+    private CalcMovesSouth(board: GameTile[][], rookRow: number, rookCol: number){
+
+    }
+
+    private CalcMovesEast(board: GameTile[][], rookRow: number, rookCol: number){
+
+    }
+
+    private CalcMovesWest(board: GameTile[][], rookRow: number, rookCol: number){
+
+    }
+
+    public markAsMoveOption(): void {
+        this.possibleMoves.forEach(tile => {
+            tile.isMoveOption = true;
+        });
     }
 }
