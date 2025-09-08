f3cc288f19d5e4336eef68d15c27aaa12c8e9ec9
Malik0502 <65547172+Malik0502@users.noreply.github.com>
2025-09-08 14:09:26 +0200

    Feat: not working knight movement

--- src/scripts/game/pieces/knight.ts
+++ src/scripts/game/pieces/knight.ts
@@ -27,10 +27,77 @@ export class Knight implements IPiece{
     }
     
     calcPossibleMoves(board: GameTile[][]){
-        throw new Error("Method not implemented.");
+        this.possibleMoves = [];
+
+        const directionNorth: number = -2;
+        const directionSouth: number = 2;
+        const directionEast: number = 2;
+        const directionWest: number = -2;
+
+        this.calcVerticalMoves(board, directionNorth);
+        this.calcVerticalMoves(board, directionSouth);
+
+        this.calcHorizontalMoves(board, directionEast);
+        this.calcHorizontalMoves(board, directionWest);
+
+        this.markAsMoveOption();
+        console.log(this.possibleMoves)
     }
+
+
+    calcVerticalMoves(board: GameTile[][], direction: number){
+        const currentTile: GameTile = this.currentTile;
+
+        let moves: GameTile[] = [];
+
+        if(currentTile.row < 2 && direction == -2) return;
+        if(currentTile.row > 6 && direction == 2) return;
+
+        const verticalRight: GameTile | undefined = currentTile.col < 7 ? board[currentTile.row += direction][currentTile.col + 1] : undefined; 
+        const verticalLeft: GameTile | undefined = currentTile.col > 1 ? board[currentTile.row += direction][currentTile.col -1] : undefined;
+    
+        moves.push(verticalRight!);
+        moves.push(verticalLeft!);
+
+        for (let index = 0; index < moves.length; index++) {
+            const element = moves[index];
+            
+            if(!element) continue;
+
+            if(currentTile.isOccupied && currentTile.currentPiece!.color == this.color) continue;
+
+            this.possibleMoves.push(element);
+        }
+    }
+
+    calcHorizontalMoves(board: GameTile[][], direction: number){
+        const currentTile: GameTile = this.currentTile;
+
+        let moves: GameTile[] = [];
+
+        if(currentTile.col < 2 || currentTile.col > 6) return;
+
+        const horizontalRight: GameTile | undefined = currentTile.row < 7 ? board[currentTile.row + 1][currentTile.col += direction] : undefined; 
+        const horizontalLeft: GameTile | undefined = currentTile.row > 1 ? board[currentTile.row - 1][currentTile.col += direction] : undefined;
+    
+        moves.push(horizontalRight!);
+        moves.push(horizontalLeft!);
+
+        for (let index = 0; index < moves.length; index++) {
+            const element = moves[index];
+            
+            if(!element) continue;
+
+            if(currentTile.isOccupied && currentTile.currentPiece!.color == this.color) continue;
+
+            this.possibleMoves.push(element);
+        }
+    }
+
     
     markAsMoveOption(): void {
-        throw new Error("Method not implemented.");
+        this.possibleMoves.forEach(tile => {
+            tile.isMoveOption = true;
+        });
     }
 }
